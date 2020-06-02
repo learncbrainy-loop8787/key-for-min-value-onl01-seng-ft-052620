9739086c8223174a292afcd3279d68c80848ce49
korey ko <koreykendrick1@gmail.com>
2020-06-02 10:31:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,10 @@ def key_for_min_value(name_hash)
   lowest_vaule = 0
   lowest_key = nil
 name_hash.each do |key, vaule| 
-  
+  if lowest_vaule == 0 || vaule < lowest_vaule 
+    lowest_vaule = vaule 
+    lowest_key = key 
+  end
+end
+lowest_key 
 end
